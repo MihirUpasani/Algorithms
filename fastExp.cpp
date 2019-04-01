22e48a57e227a6447532e5dd41b7bbcf83830276
MihirUpasani <u.mihir8395@gmail.com>
2019-04-01 10:16:36 +0530

    fast_exp(c++) code imp

--- fastExp.cpp
+++ fastExp.cpp
@@ -6,38 +6,55 @@
 #include <cmath>
 using namespace std;
 
-long int mfer(long int number, long int power, long int quotient);
-int bin(long int power);
+unsigned long int mfer(unsigned long int number, unsigned long int power, unsigned long int quotient);
 
-int bin(long int power)
+unsigned long int mfer(unsigned long int number, unsigned long int power, unsigned long int quotient)
 {
-    // array to store binary number
-    int binaryNum[100];
+    cout << "Called" << endl;
 
-    // counter for binary array
-    int i = 0;
-    while (power > 0)
-    {
-
-        // storing remainder in binary array
-        binaryNum[i] = power % 2;
-        power = power / 2;
-        i++;
-    }
-    return binaryNum;
-}
-
-long int mfer(long int number, long int power, long int quotient)
-{
-    map<long int, long int> memo;
+    map<unsigned long int, unsigned long int> memo;
     memo.insert({1, number});
-    long int start_with = 1;
-    long int new_insert = 1;
+    unsigned long int start_with = 1;
+    unsigned long int new_insert = 1;
+    unsigned long int remainder = 1;
+    int bin_rep[100], bin_len, i;
+
+    cout << "Decalrations done" << endl;
+
     while (start_with <= power)
     {
+        cout << "start_with : " << start_with << endl;
         new_insert = memo.at(start_with);
         new_insert = (new_insert * new_insert) % quotient;
         start_with = start_with * 2;
+        // mfer, you forgot to insert
+        memo.insert({start_with, new_insert});
     }
-    int bin_rep[100] = bin(power)
+
+    cout << "MemTable calculated" << endl;
+
+    bin_len, i = 0;
+    while (power > 0)
+    {
+        bin_rep[i] = power % 2;
+        power = power / 2;
+        i++;
+        bin_len++;
+    }
+
+    cout << "Power binary rep done" << endl;
+
+    for (i = bin_len; i >= 0; i--)
+    {
+        if (bin_rep[i] == 1)
+        {
+            remainder = remainder * memo.at(pow(2, i));
+        }
+    }
+    return (remainder % quotient);
+}
+
+int main()
+{
+    cout << mfer(24, 34, 215) << endl;
 }
